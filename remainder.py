7cdffce9d76311f4e78e28c15a834f45f9bed4da
sallada123 <127488704+sallada123@users.noreply.github.com>
2023-03-10 20:04:23 -0800

    Add files via upload

---
+++ remainder.py
@@ -0,0 +1,13 @@
+#Asking the user for inputs 
+print('Please enter the dividend :')
+dividend = int(input(''))
+print('Please enter the divisor :')
+divisor = int(input(''))
+
+while True:
+    #checking divisibility
+    if dividend % divisor == 0:
+        print('Success!',divisor,'divides the dividend')
+        break
+    print(divisor,'does not divide the dividend')
+    break
